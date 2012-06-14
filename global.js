da5d5345e84a4675b11b18d7e36e684e7d4f881b
Alex Ninneman <ninnemana@gmail.com>
2012-06-14 16:49:56 -0500

    finished lookup and added search functionality

--- global.js
+++ global.js
@@ -8,33 +8,75 @@ $(function(){
 
 	// User selects mount [ Rear Mount or Front Mount]
 	$(document).on('change','select.mount',function(){
-		mount = $(this).val();
+		// Reset lookup to only dislay mount
+		$('select.year, select.make, select.model, select.style').remove();
+		year = make = model = style = '';
+
+		mount = $(this).val(); // Retrieve the selected mount
 		if(mount.length > 0){
-			getYear(mount);
+			getYear(mount); // Retrieve years from the API
 		}
 	});
 
 	// User selects vehicle year
 	$(document).on('change','select.year',function(){
-		year = $(this).val();
+
+		// Reset lookup to only display mount and year
+		$('select.make, select.model, select.style').remove();
+		make = model = style = '';
+
+		year = $(this).val(); // Retrieve select year
 		if(year.length > 0){
-			getMake(mount, year);
+			getMake(mount, year); // Get vehicle makes from the API
 		}
 	});
 
+	// User selects vehicle make
 	$(document).on('change','select.make',function(){
-		make = $(this).val();
+
+		// Reset the lookup to only display mount, year, and make
+		$('select.model, select.style').remove();
+		model = style = '';
+
+		make = $(this).val(); // Retrieve selected make
 		if(make.length > 0){
-			getModel(mount, year, make);
+			getModel(mount, year, make); // Get vehicle models from the API
 		}
 	});
 
+	// User select vehicle model
 	$(document).on('change','select.model',function(){
-		model = $(this).val();
+
+		// Reset the lookup to only display mount, year, make, and model
+		$('select.style').remove();
+		style = '';
+
+		model = $(this).val(); // Retrieve the selected model
 		if(model.length > 0){
-			getStyle(mount, year, make, model);
+			getStyle(mount, year, make, model); // Get the vehicle styles from the API
 		}
 	});
+
+	// User selects vehicle style
+	$(document).on('change','select.style',function(){
+		// Remove part results
+		$('div.part').remove();
+
+		style = $(this).val(); // Retrieve the selected style
+		if(style.length > 0){
+			getParts(mount, year, make, model, style); // Get the parts that match the current vehicle configuration
+		}
+	});
+
+	// Handle the search submission
+	$(document).on('click','div.search-box button',function(e){
+		e.preventDefault();
+		var query = $('div.search-box input[type=search]').val();
+		search(query);
+	});
+
+	// Get Parent Categories
+	getParents();
 });
 
 function getYear(mount){
@@ -123,3 +165,150 @@ function getStyle(mount, year, make, model){
 		}
 	});
 }
+
+function getParts(mount, year, make, model, style){
+	var url = 'http://api.curtmfg.com/v2/GetParts?dataType=json';
+	url += '&mount=' + encodeURIComponent(mount);
+	url += '&year=' + encodeURIComponent(year);
+	url += '&make=' + encodeURIComponent(make);
+	url += '&model=' + encodeURIComponent(model);
+	url += '&style=' + encodeURIComponent(style);
+	$.ajax({
+		url: url,
+		type: 'GET',
+		dataType: 'json',
+		success: function(data, status, xhr){
+			$.each(data,function(iterator, part){
+				var html = displayPart(part);
+				$('section.content').append(html);
+			});
+		},
+		error: function(xhr, status, error){
+			document.write('holy wow batman! we hit da errors.');
+		}
+	});
+}
+
+function getPart(id, callback){
+	var url = 'http://api.curtmfg.com/v2/GetPart?dataType=json';
+	url += '&partID=' + id;
+	$.ajax({
+		url: url,
+		type: 'GET',
+		dataType: 'json',
+		success: function(data, status, xhr){
+			if(data !== undefined && data.partID !== undefined){
+				callback(data);
+			}else{
+				callback({});
+			}
+		},
+		error: function(xhr, status, error){
+			callback({});
+		}
+	});
+}
+
+function search(query){
+	var url = 'http://api.curtmfg.com/v2/Search?dataType=json';
+	url += '&search_term=' + query;
+	$.ajax({
+		url: url,
+		type: 'GET',
+		dataType: 'json',
+		beforeSend: function(){
+			$('section.content').html('<h4>We\'re searching for your results</h4>');
+		},
+		success: function(data,status,xhr){
+			$('section.content').html('');
+			if(data.length === 0){
+				$('section.content').html('<h4>Sorry we couldn\'t find any results for your query');
+			}
+			
+			$.each(data,function(iterator, result){
+				if(result.partID !== null){
+					getPart(result.partID,function(part){
+						if(part.partID !== undefined){
+							var html = displayPart(part);
+							$('section.content').append(html);
+						}
+					});
+				}
+			});
+		},
+		error: function(xhr, status, error){
+			$('section.content').text('aw buddy, i\'m sorry. we went ahead and screwed this one up for you!');
+		}
+	});
+}
+
+function displayPart(part){
+	var partID = part.partID,
+		desc = part.shortDesc,
+		price = part.listPrice,
+		html = '',
+		img_arr = [];
+
+	$.each(part.images,function(pi_iterator, image){
+		if(image.size === 'Grande' && image.sort === 'a'){
+			img = image.path;
+		}else if(image.size === 'Grande' && image.sort !== 'a'){
+			img_arr.push(image.path);
+		}
+	});
+
+	html = '<div class="part">';
+	html += '<h2>' + desc + '</h2>';
+	html += '<p>Part #' + partID + '</p>';
+
+	html += '<div class="img-container">';
+	html += '<img src="' + img + '" class="primary" />';
+	if(img_arr.length > 0){
+		html += '<div class="sub-images">';
+			$.each(img_arr,function(img_iterator, sub){
+				html += '<img src="' + sub + '" class="sub" />';
+			});
+		html += '</div>';
+	}
+	html += '</div>';
+
+	html += '<div class="attributes">';
+	$.each(part.attributes,function(i, attr){
+		html += '<p><strong>' + attr.key + '</strong> ' + attr.value + '</p>';
+	});
+	html += '</div>';
+
+	html += '<div class="pricing">';
+	html += '<span class="price">' + price + '</span>';
+	html += '<button>Buy Me</button>';
+	html += '</div>';
+
+	html += '<div class="clearfix"></div>';
+	html += '</div>';
+
+	return html;
+}
+
+function getParents(){
+	var url = 'http://api.curtmfg.com/v2/GetParentCategories?dataType=json';
+	$.ajax({
+		url: url,
+		type: 'GET',
+		dataType:'json',
+		success:function(data, status, xhr){
+			$.each(data,function(iterator,parent){
+				var html = '<div class="category">';
+
+
+				html += '</div>';
+
+			});
+		},
+		error: function(xhr, status, error){
+			$('section.content').html('<span>Bummer, we couldn\'t get any categories');
+		}
+	});
+}
+
+
+
